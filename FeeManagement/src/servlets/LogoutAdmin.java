2bce4f654588bd3aba1c057dd2daa7b0ae7bb67a
Admin <Admin@Admin-PC>
2017-11-30 20:52:35 +0530

    1st commit

---
+++ FeeManagement/src/servlets/LogoutAdmin.java
@@ -0,0 +1,38 @@
+package servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+@WebServlet("/LogoutAdmin")
+public class LogoutAdmin extends HttpServlet {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter out=response.getWriter();
+		
+		out.println("<!DOCTYPE html>");
+		out.println("<html>");
+		out.println("<head>");
+		out.println("<title>Admin Logout</title>");
+		out.println("</head>");
+		out.println("<body>");
+		request.getRequestDispatcher("navhome.html").include(request, response);
+		
+		out.println("<h1>Admin Logout Success</h1>");
+		HttpSession session=request.getSession();
+		session.invalidate();
+		request.getRequestDispatcher("AdminLoginForm.html").include(request, response);
+		
+		request.getRequestDispatcher("footer.html").include(request, response);
+		out.println("</body>");
+		out.println("</html>");
+		
+		out.close();
+	}
+
+}
---
+++ FeeManagement/src/servlets/SearchStudentForm.java
@@ -0,0 +1,33 @@
+package servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+@WebServlet("/SearchStudentForm")
+public class SearchStudentForm extends HttpServlet {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter out=response.getWriter();
+		out.println("<!DOCTYPE html>");
+		out.println("<html>");
+		out.println("<head>");
+		out.println("<title>Search Student Form</title>");
+		out.println("</head>");
+		out.println("<body>");
+		request.getRequestDispatcher("navaccountant.html").include(request, response);
+		
+		request.getRequestDispatcher("SearchStudentForm.html").include(request, response);
+		
+		out.println("</div>");
+		request.getRequestDispatcher("footer.html").include(request, response);
+		out.println("</body>");
+		out.println("</html>");
+		
+		out.close();
+	}
+}
